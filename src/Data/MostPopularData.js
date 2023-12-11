c71d95a5341cf33a2e53a05efbc658136a8492e8
Raneem2000 <rdasouki6@gmail.com>
2023-12-11 21:01:06 +0300

    fix images

--- src/Data/MostPopularData.js
+++ src/Data/MostPopularData.js
@@ -1,13 +1,19 @@
-import popular_01 from '../assets/images/logo.png';
+import popular_01 from '../assets/images/b.jpg';
+import popular_02 from '../assets/images/c.jpg';
+import popular_03 from '../assets/images/d.jpg';
+import popular_04 from '../assets/images/e.jpg';
+import popular_05 from '../assets/images/f.jpg';
+import popular_06 from '../assets/images/g.jpg';
+
 
 const MostPopularData = [
-        { id :'most_popular_item_0' , image:'https://picsum.photos/200' , title: 'eioio' ,category :'erejif' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_1' , image: 'https://picsum.photos/201' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_2' , image: 'https://picsum.photos/202', title: 'jdlfkldjf' ,category :'dfjkdjfh' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_3' , image: 'https://picsum.photos/203' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_4' , image: 'https://picsum.photos/204' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_5' , image: 'https://picsum.photos/205' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_6' , image: 'https://picsum.photos/206' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
-        { id :'most_popular_item_7' , image: 'https://picsum.photos/207' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_0' , image: popular_01 , title: 'eioio' ,category :'erejif' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_1' , image: popular_02 , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_2' , image: popular_03, title: 'jdlfkldjf' ,category :'dfjkdjfh' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_3' , image: popular_04 , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_4' , image: popular_05 , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        { id :'most_popular_item_5' , image: popular_06 , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        // { id :'most_popular_item_6' , image: 'https://picsum.photos/206' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
+        // { id :'most_popular_item_7' , image: 'https://picsum.photos/207' , title: 'fortnite' ,category :'sandbox' , rate: '3.8', download:'3.5M'},
     ]
 export default MostPopularData
